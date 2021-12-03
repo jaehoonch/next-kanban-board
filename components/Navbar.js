b86fd8b36cc88bd7bcbae51cf0400d7e7584243e
Jaehoon Chung <jaehoonch@hotmail.com>
2021-12-03 16:57:15 +0900

    added basic components and page layouts

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="py-4 border-b border-gray-100 shadow">
+            This is the main navigation
+        </div>
+    )
+}
+
+export default Navbar
